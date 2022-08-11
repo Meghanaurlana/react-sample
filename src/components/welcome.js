36a77eae70f604048dbab21f8677c29bf1fc401d
meghana <meghana.urlana@gmail.com>
2022-08-11 17:03:59 +0530

    new

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -2,8 +2,9 @@ import { useState } from "react"
 export const Welcome = () => {
     const [message, setMessage] = useState('new message')
     return (
-    <div>
-        <h1>{message}</h1>
-        <button onClick={()=>setMessage('welcome Meghana')}>click</button>
-    </div>
-)}
+        <div>
+            <h1>{message}</h1>
+            <button onClick={() => setMessage('welcome Meghana')}>click</button>
+        </div>
+    )
+}
